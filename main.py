9828ca57d0044c4f73f16721c9fa8b9866a5ca09
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-19 16:07:27 +0200

    + update #9486

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from pydub import AudioSegment
+from pydub.playback import play
+from playsound import playsound 
 
 class Window(Frame):
 
@@ -10,12 +13,17 @@ class Window(Frame):
         self.pack(fill=BOTH, expand=1)
 
         # create exit button
-        exit_button = Button(self, text="Exit", command=self.click_exit_button) 
+        exit_btn = Button(self, text="Exit", command=self.click_exit_btn) 
+        exit_btn.place(x=0, y=0)
+        
+        # create play button
+        play_btn = Button(self, text="Play", command=self.click_play_btn)
+        play_btn.place(x=100,y=100)
 
-        # place button at (0,0)
-        exit_button.place(x=0, y=0)
+    def click_play_btn(self):
+        playsound("test.mp3")
 
-    def click_exit_button(self):
+    def click_exit_btn(self):
         exit()
 
 
