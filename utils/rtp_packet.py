d45d4b8de49e0a5ffea7dd17ad09d84e1ac3ec11
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-20 10:28:49 +0200

    + update #31482

--- utils/rtp_packet.py
+++ utils/rtp_packet.py
@@ -1,1 +1,34 @@
 
+
+
+
+
+class InvalidPacketException(Exception):
+    pass
+
+class RTPacket:
+    # default header info
+
+    HEADER_SIZE = 12    # bytes
+    VERSION     = 0b10  # 2 bits .> current version 2
+    PADDING     = 0b0   # 1 bit
+    EXTENSION   = 0b0   # 1 bit
+    CC          = 0x0   # 4 bits
+    MARKER      = 0b0   # 1 bit
+    SSRC        = 0x00000000 # 32 bits
+
+    class TYPE: 
+        MJPEG = 26
+
+    def __init__(self, 
+                payload_type: int = None,
+                sequence_number: int = None,
+                timestamp: int = None,
+                payload:bytes =None):
+
+        self.paylaod        = payload
+        self.payload_type   = payload_type
+        self.sequence_number= sequence_number
+        self.timestamp      = timestamp
+
+
