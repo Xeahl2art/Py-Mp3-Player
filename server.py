ff7a622c2d7615679eaf43d2cbed9a615bce13fd
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 10:25:13 +0200

    + update #14174

--- server.py
+++ server.py
@@ -5,4 +5,20 @@ from  threading import Thread
 from utils.rtp_packet import RTPPacket
 
 
+from pydub import AudioSegment
+from pydub.playback import play
+
+song = AudioSegment.from_mp3(".test.mp3")
+
+def wait_con():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = "localhost", 1234
+
+    s.bind(address)
+    s.listen(1)
+    print(f"Listening for con.. ")
+    conc, client_addr = s.accept()
+    print(f"conc {conc} client_addr {client_addr}")
+    
+wait_con()
 
