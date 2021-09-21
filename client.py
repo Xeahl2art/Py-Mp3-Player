f03139c539c5d3ae604195fb03d99a2a479b0be1
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 19:44:17 +0200

    + update #9859

--- client.py
+++ client.py
@@ -1,18 +1,7 @@
-import socket
-import json
-import pickle
+import subprocess
 
-ip = "127.0.0.1" # input("IP-Adresse: ")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((ip, 1234))
+client = subprocess.Popen([
+        "ffplay",
+        "rtp://127.0.0.1",
 
-data = {'x':[1,2,3,5,6,7]}
-s.close()
-#try: 
-#    while True: 
-#        data = pickle.dumps(data)
-#        s.send(data) 
-#        antwort = s.recv(1024) 
-#        print("[{}] {}".format(ip, antwort.decode())) 
-#finally: 
-#    s.close()
+    ])
