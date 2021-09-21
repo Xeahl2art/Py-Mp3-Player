2cb194fef20e5daea25fa89e4a8e050cdf2c3e72
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 14:17:30 +0200

    + update #13343

--- ffplay_chunks2.py
+++ ffplay_chunks2.py
@@ -4,22 +4,44 @@ import subprocess
 in_filename = '.test.mp3'
 
 with open('.test.mp3','rb') as f:
-         chunk = f.read(4096) # 4Kib
+         chunk = f.read(7896687) # 4Kib
 process1 = (
     ffmpeg
-    .input("pipe:")
+    .input('pipe:', format='mp3')
     # (filters/etc. go here)
     .output('pipe:', format='mp3')
     .run_async(pipe_stdout=True)
 )
-process1.stdin(chunk)
+
+myproc = subprocess.Popen([
+
+    'ffmpeg',
+    '-f mp3',
+    '-i pipe:',
+    '-o pipe:',
+    '-f mp3',
+    ],
+    shell=True,
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+)
+
+
 process2 = subprocess.Popen(
     [
         'ffplay',
         '-f', 'mp3',
         '-i', 'pipe:',
     ],
-    stdin=process1.stdout,
+    stdin=subprocess.PIPE,
 )
+
+#myproc.wait()
+#myproc.stdin.write(chunk)
+out = process1.communicate(chunk)[0]
 process1.wait()
+
+process2.communicate(out)[0]
 process2.wait()
+
+
