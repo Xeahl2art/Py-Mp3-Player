fed51f990563041b7ddea0b0d5e5764daaa4c0f9
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 15:53:30 +0200

    + update #6266

--- again.py
+++ again.py
@@ -1,13 +1,17 @@
  
 import subprocess
 # init command
-ffmpeg_command = ["ffmpeg", "-i", ".test.mp3",
+with open('.test.mp3','rb') as f:
+         chunk = f.read(7896687) # 4Kib
+
+
+ffmpeg_command = ["ffmpeg", "-i", "pipe:2",
          "-ab", "128k", "-acodec", "pcm_s16le", "-ac", "0",  "-map",
          "0:a", "-map_metadata", "-1", "-sn", "-vn", "-y",
          "-f", "wav", "pipe:1"]
 
  # excute ffmpeg command
-pipe = subprocess.run(ffmpeg_command,
+pipe = subprocess.Popen(ffmpeg_command,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE,
                        bufsize=10**8)
@@ -26,8 +30,9 @@ def write_(file):
     with open(".test2.wav",'wb') as f:
         f.write(out)
 
-out = pipe.stdout
+print(chunk[:10])
+out = pipe.communicate(input=chunk)[0]
 #write_(out)
 print(pipe.stderr)
-process2.communicate(out)
-process2.wait()
+#process2.communicate(out)
+#process2.wait()
