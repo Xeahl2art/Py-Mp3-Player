1656afa510c504036cf9c97de8236796e11e8ffd
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 15:20:26 +0200

    + update #14034

--- again.py
+++ again.py
@@ -2,7 +2,7 @@
 import subprocess
 # init command
 ffmpeg_command = ["ffmpeg", "-i", ".test.mp3",
-         "-ab", "128k", "-acodec", "pcm_s16le", "-ac", "0", "-ar", "-map",
+         "-ab", "128k", "-acodec", "pcm_s16le", "-ac", "0",  "-map",
          "0:a", "-map_metadata", "-1", "-sn", "-vn", "-y",
          "-f", "wav", "pipe:1"]
 
@@ -12,19 +12,22 @@ pipe = subprocess.run(ffmpeg_command,
                        stderr=subprocess.PIPE,
                        bufsize=10**8)
 
- # debug
 
 process2 = subprocess.Popen(
-     [
-         'ffplay',
-         '-f', 'wav',
-         '-i', 'pipe:',
-     ],
-     stdin=subprocess.PIPE,
+    [   
+        'ffplay',
+        '-f', 'wav',
+        '-i', 'pipe:',
+    ],  
+    stdin=subprocess.PIPE,
 )
 
+def write_(file):
+    with open(".test2.wav",'wb') as f:
+        f.write(out)
 
 out = pipe.stdout
-process2.communicate(out)[0]
-
-
+#write_(out)
+print(pipe.stderr)
+process2.communicate(out)
+process2.wait()
