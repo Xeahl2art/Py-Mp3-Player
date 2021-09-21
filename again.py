aec207298b20bcd5e0026d54c2b57c2c34dfa155
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 17:03:25 +0200

    + update #28742

--- again.py
+++ again.py
@@ -5,33 +5,36 @@ with open('.test.mp3','rb') as f:
          chunk = f.read(7896687) # 4Kib
 
 
-ffmpeg_command = ["ffmpeg", "-i", "pipe:2",
+ffmpeg_command = ["ffmpeg", "-i", "pipe:0",
          "-ab", "128k", "-acodec", "pcm_s16le", "-ac", "0",  "-map",
          "0:a", "-map_metadata", "-1", "-sn", "-vn", "-y",
-         "-f", "wav", "pipe:1"]
+         "-f", "wav", "pipe:2"]
 
  # excute ffmpeg command
 pipe = subprocess.Popen(ffmpeg_command,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE,
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
                        )
 
 
-process2 = subprocess.Popen(
-    [   
-        'ffplay',
-        '-f', 'wav',
-        '-i', 'pipe:',
-    ],  
-    stdin=subprocess.PIPE,
-)
-
+#process2 = subprocess.Popen(
+#    [   
+#        'ffplay',
+#        '-f', 'wav',
+#        '-i', 'pipe:3',
+#    ],  
+#    stdin=subprocess.PIPE,
+#)
+#
 def write_(file):
     with open(".test2.wav",'wb') as f:
         f.write(file)
-#pipe.stdin = chunk
-out = pipe.communicate(input=chunk.hex())[0]
-#write_(out)
+
+print(len(chunk))
+pipe.wait()
+out = pipe.communicate(input=chunk)[0]
+write_(out)
 print(pipe.stderr)
-process2.communicate(out)
+#process2.communicate(out)
 #process2.wait()
