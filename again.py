4d88eb882b58c8e384347971f4859825e40570a2
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-21 17:13:20 +0200

    + update #29072

--- again.py
+++ again.py
@@ -5,36 +5,35 @@ with open('.test.mp3','rb') as f:
          chunk = f.read(7896687) # 4Kib
 
 
-ffmpeg_command = ["ffmpeg", "-i", "pipe:0",
+ffmpeg_command = ["ffmpeg", "-i", "-",
          "-ab", "128k", "-acodec", "pcm_s16le", "-ac", "0",  "-map",
          "0:a", "-map_metadata", "-1", "-sn", "-vn", "-y",
-         "-f", "wav", "pipe:2"]
+         "-f", "wav", "-"]
 
  # excute ffmpeg command
 pipe = subprocess.Popen(ffmpeg_command,
-                        stdin=subprocess.PIPE,
+                        stdin =subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                        )
 
 
-#process2 = subprocess.Popen(
-#    [   
-#        'ffplay',
-#        '-f', 'wav',
-#        '-i', 'pipe:3',
-#    ],  
-#    stdin=subprocess.PIPE,
-#)
-#
+process2 = subprocess.Popen(
+    [   
+        'ffplay',
+        '-f', 'wav',
+        '-i', '-',
+    ],  
+    stdin=subprocess.PIPE,
+)
+
 def write_(file):
     with open(".test2.wav",'wb') as f:
         f.write(file)
 
 print(len(chunk))
-pipe.wait()
 out = pipe.communicate(input=chunk)[0]
 write_(out)
 print(pipe.stderr)
-#process2.communicate(out)
-#process2.wait()
+process2.communicate(out)
+process2.wait()
