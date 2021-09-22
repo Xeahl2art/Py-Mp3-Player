78a779d6b4828faae76b3952e39d9e420363ccf7
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-22 11:39:33 +0200

    + update #19422

--- rtp_server.py
+++ rtp_server.py
@@ -1,11 +1,13 @@
 import socket as s
 from time import sleep
-
+import io
+io.DEFAULT_BUFFER_SIZE =131072
 localIp = "localhost"
 port    = 1234
 buffers = 1024
 
 udps = s.socket(s.AF_INET, s.SOCK_DGRAM)
+#udps.setsockopt(s.SOL_SOCKET,s.SO_SNDBUF,131072)
 udps.bind((localIp, port))
 
 recv=None
@@ -21,16 +23,16 @@ while recv is None:
     #print(clientMsg)
     #print(clientIP)
 
-    with open(".test.mp3","rb") as f:
+    with open(".test_rtp","rb") as f:
         c_adress = ("localhost",1235)
-        with open("rtp_info.sdp","rb")as f2:
+        with open("rtp_info2.sdp","rb")as f2:
             sdp = f2.read()
            
             while 1:
-                pkg = f.read(1024)
+                pkg = f.read(1024*8)
                 if not pkg:
                     break
-                udps.sendto( sdp , c_adress)
+                #udps.sendto( sdp , c_adress)
                 udps.sendto( pkg , c_adress)
-                #sleep(0.2)
+                #sleep(0.15)
 
