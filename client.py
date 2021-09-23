fe797c56a652103c9f60d238d4b0c89f5dccf061
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-23 09:23:06 +0200

    + update #31667

--- client.py
+++ client.py
@@ -5,19 +5,25 @@ from time import sleep
 
 class Client_ffplay():
     c_ffplay = subprocess.Popen([
-        "ffplay", "-f", "wav",
+        "ffplay", "-f", "wav","-loglevel","quiet",
          "-nodisp", "-autoexit","-vn",
-         "rtp://127.0.0.1:1235",
+         "rtp://127.0.0.1:1237",
         ],
         stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
         )
 
     async def start(self):
         self.c_ffplay.wait()
+        sleep(0.5)
     async def start2(self):
+        print("sd")
         while True:
+            sleep(0.2)
             if self.c_ffplay is not None:
                 out = self.c_ffplay.stdout
+                print(out)
+                out = self.c_ffplay.communicate(input=b'6\x0b\xe9\xfdA')[0]
                 print(out)
                 print("sdasd")
     
