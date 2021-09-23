511ac23a09db8f3f42abd24c2a088343271a0b1a
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-23 09:01:45 +0200

    + update #18216

--- client.py
+++ client.py
@@ -1,30 +1,35 @@
 import subprocess
+import asyncio
 import keyboard
 from time import sleep
 
 class Client_ffplay():
-    client = subprocess.Popen([
-        "ffplay",
-         "-nodisp", "-autoexit",
-         "rtp://127.0.0.1:1234",
+    c_ffplay = subprocess.Popen([
+        "ffplay", "-f", "wav",
+         "-nodisp", "-autoexit","-vn",
+         "rtp://127.0.0.1:1235",
         ],
-        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
         )
 
-    def start(self):
-        self.key_listener()
-        self.client.wait()
-        
-    async def key_listener(self):
+    async def start(self):
+        self.c_ffplay.wait()
+    async def start2(self):
         while True:
-            sleep(0.5)
-            if keyboard.is_press_and_release("q"):
-                print("asd")
-                self.client.communicate(input='q')
+            if self.c_ffplay is not None:
+                out = self.c_ffplay.stdout
+                print(out)
+                print("sdasd")
+    
+    async def main(self):
+        
+        await asyncio.gather(
+                asyncio.create_task(self.start()),
+                asyncio.create_task(self.start2())
 
-
+            )
 
     
 
 client = Client_ffplay()
-client.start()
+asyncio.run(client.main())
