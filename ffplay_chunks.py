7a07ea704cf0f6d93789c3e104a72caefdbf711f
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-24 20:05:55 +0200

    + update #1622

--- ffplay_chunks.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-with open('.test.mp3','rb') as f:
-        chunk = f.read(4096) # 4Kib
-ffplay_proc = subprocess.Popen('ffplay', shell=True, stdin=subprocess.PIPE)
-ffplay_proc.stdin.write(chunk)
-
-
-
