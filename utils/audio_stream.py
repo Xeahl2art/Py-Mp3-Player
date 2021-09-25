cc064ec09cba2849b40e48d8f43dc7c276083b9d
Xeahl2art <xeahl_art2@protonmail.com>
2021-09-25 09:38:15 +0200

    bug #1

--- utils/audio_stream.py
+++ utils/audio_stream.py
@@ -51,79 +51,89 @@ class AudioStream:
         return frame
 
     def get_header(self): # -1 broke   
-        self.find_pattern_in_open_file(pattern=[b'\xff',[b'\xfb',b'\xfa'])
+        self.find_pattern_in_open_file(pattern=[b'\xff',[b'\xfb',b'\xfa'], self._ )
 
-    def find_pattern_in_open_file(self, pattern,open_file )
+    def find_pattern_in_open_file(self, pattern, open_file )
+        found_pattern =['']*len(pattern)    
         for i in range(0,len(pattern)):
-            pats =['']*len(pattern)    
-            length_in_pats =[ len(pattern[i]) for in range(0,len(pattern))
-            pats[i] = file.read(1)
-    
-            while pats[i] != pattern[i]: 
-                pats[i] = open_file.read(1)
+                bys =['']*len(pattern)    
+                length_in_pat =[ len(pattern[i]) for in range(0,len(pattern))
+                bys[i] = file.read(1)
+                if bys[i-1] == bys[i]:
+                    i -=1
+                    continue
+                   
+                for pats in pattern[i]:
+                    while bys[i] != pats: 
+                        bys[i] = open_file.read(1)
+                        if bys[i] == '':
+                            break
+                    else:
+                        found_pattern[i] = pats
 
-            by2 = self._stream.read(1)
-        while by2 == b'\xff':
-                by1 = by2 
-                by2 = self._stream.read(1)
-                
-            if frame2 == b'\xfb'or frame2 == b'\xfa' : # 0b1111011 = 251 -> mp3
-                frame3 =self._stream.read(1)
-                frame4 =self._stream.read(1)
-                frame = frame1+frame2+frame3+frame4
-                print( bin( frame[0] ) )
-                print( str( bin(frame[1])))
-                print( str(bin(frame[2])))
-                print( str(bin(frame[3])))
-                print("frame: "+str( [ hex(q) for q in frame[:9] ] ))
-                if frame3 == b'\x94':
-                    self.offset =4
-                # third byte 
-                q = bin(frame[2])
-                print("frame: "+str(frame[2])) 
-                dif= 8 - (len(q)-2)
-                q = q if dif==0 else '0b'+'0'*dif+q[2:]                  
-                print("q: "+str(q))
-
-                #bitrate
-                bq = q[:6]
-                BR=[64,32 ,40 ,48 ,56 ,64,80 ,96 ,112 ,128 ,160 ,192, 224 ,256, 320,-1] 
-                self.bitrate =BR[int(bq,2)]
-                print("bq:"+str(bq))
-                print("bitrate: "+str(self.bitrate))
-
-                #fq
-                fq = '0b'+q[6:8]
-                Hz = [44100,48000,32000,-1]
-                print("fq: "+str(fq))
-                w = int(fq,2)
-                print("w: "+ str(w))
-                self.freq = Hz[w]
-                print("freq: "+str(self.freq))
-               
-                #padding bit
-                qp =q[-1]
-
-                if self.freq < 0 or self.bitrate < 0:
-                    self.i +=1
-                    print("\n")
-                    print("-----------------------------------")
-                    print("frame: "+str( [ hex(q) for q in frame[:4] ] )) 
-                    print("fq: "+str(fq))
-                    print("freq: "+str(self.freq))
-                    print("bq:"+str(bq))
-                    print("bitrate: "+str(self.bitrate))
-                    print("Header skip: "+ str(self.i))
-                    print("------------------------------------")
-                    print("\n")
-                    #input()
-                    return -1
-                self.header = frame
-                 
-                print()
-                print()
-                return 1
-        return -1
+#                        
+#                pats = self._stream.read(1)
+#        while by2 == b'\xff':
+#                by1 = by2 
+#                by2 = self._stream.read(1)
+#                
+#            if frame2 == b'\xfb'or frame2 == b'\xfa' : # 0b1111011 = 251 -> mp3
+#                frame3 =self._stream.read(1)
+#                frame4 =self._stream.read(1)
+#                frame = frame1+frame2+frame3+frame4
+#                print( bin( frame[0] ) )
+#                print( str( bin(frame[1])))
+#                print( str(bin(frame[2])))
+#                print( str(bin(frame[3])))
+#                print("frame: "+str( [ hex(q) for q in frame[:9] ] ))
+#                if frame3 == b'\x94':
+#                    self.offset =4
+#                # third byte 
+#                q = bin(frame[2])
+#                print("frame: "+str(frame[2])) 
+#                dif= 8 - (len(q)-2)
+#                q = q if dif==0 else '0b'+'0'*dif+q[2:]                  
+#                print("q: "+str(q))
+#
+#                #bitrate
+#                bq = q[:6]
+#                BR=[64,32 ,40 ,48 ,56 ,64,80 ,96 ,112 ,128 ,160 ,192, 224 ,256, 320,-1] 
+#                self.bitrate =BR[int(bq,2)]
+#                print("bq:"+str(bq))
+#                print("bitrate: "+str(self.bitrate))
+#
+#                #fq
+#                fq = '0b'+q[6:8]
+#                Hz = [44100,48000,32000,-1]
+#                print("fq: "+str(fq))
+#                w = int(fq,2)
+#                print("w: "+ str(w))
+#                self.freq = Hz[w]
+#                print("freq: "+str(self.freq))
+#               
+#                #padding bit
+#                qp =q[-1]
+#
+#                if self.freq < 0 or self.bitrate < 0:
+#                    self.i +=1
+#                    print("\n")
+#                    print("-----------------------------------")
+#                    print("frame: "+str( [ hex(q) for q in frame[:4] ] )) 
+#                    print("fq: "+str(fq))
+#                    print("freq: "+str(self.freq))
+#                    print("bq:"+str(bq))
+#                    print("bitrate: "+str(self.bitrate))
+#                    print("Header skip: "+ str(self.i))
+#                    print("------------------------------------")
+#                    print("\n")
+#                    #input()
+#                    return -1
+#                self.header = frame
+#                 
+#                print()
+#                print()
+#                return 1
+#        return -1
     
     def get_mp3data(self,frame):
         self.frameSize = 144 * self.bitrate*1000 // (self.freq+self.pad)
